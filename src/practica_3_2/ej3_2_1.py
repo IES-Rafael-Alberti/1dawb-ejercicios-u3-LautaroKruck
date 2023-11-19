87ee5a9216f6425821efb0000b6f86da29d05dc6
Lautaro <lkru144@g.educaand.es>
2023-11-19 22:02:44 +0100

    a

--- src/practica_3_2/ej3_2_1.py
+++ src/practica_3_2/ej3_2_1.py
@@ -1,2 +1,23 @@
+"""
+Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+"""
+from src.borrar_consola import borrar_consola
+
 def pedir_divisa():
-    return input()
+    divisa = input("Introduce una divisa: ")
+    return divisa.title()
+
+def comprobar_divisa(divisa, d):
+    if divisa in d:
+        print(d[divisa])
+    else:
+        print("La divisa que ha introducido no está en el diccionario.")
+
+def main():
+    borrar_consola()
+    d = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+    divisa = pedir_divisa()
+    comprobar_divisa(divisa, d)
+
+if __name__ == "__main__":
+    main()
