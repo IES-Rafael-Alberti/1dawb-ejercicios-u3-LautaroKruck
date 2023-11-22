3429f3faf79db3ddbef35c2e1c89f62b11c55d84
Lautaro <lkru144@g.educaand.es>
2023-11-22 13:04:28 +0100

    a

---
+++ tests/Puebas_3_1/prueba_3_1_2.py
@@ -0,0 +1,10 @@
+import pytest
+from src.borrar_consola import borrar_consola
+from src.practica_3_1.ej3_1_2 import dar_lista
+
+def test_dar_lista():
+    assert dar_lista(["Matemáticas", "Física", "Química", "Historia", "Lengua"]) == "Matemáticas-Física-Química-Historia-Lengua"
+
+    assert dar_lista([]) == ""
+
+    assert dar_lista(["Matemáticas"]) == "Matemáticas"
